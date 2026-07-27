8e6284d262c513a87f9c88b43cfcfe85d6134125
Daniel Cheng <dcheng@u82z10s02.42kl.edu.my>
2026-07-27 15:56:39 +0800

    stuff

--- mazegen/__init__.py
+++ mazegen/__init__.py
@@ -1,2 +1,10 @@
-from .generator import Generator
-__all__ = ["Generator"]
+from .maze_generator import MazeGenerator
+from .solver import solve, path_to_coords
+from .renderer import render_ascii
+
+__all__ = [
+    "MazeGenerator",
+    "solve",
+    "path_to_coords",
+    "render_ascii",
+]
