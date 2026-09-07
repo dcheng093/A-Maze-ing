cb3ad074b431b6567aaadcca19b29b0958c831c2
dcheng093 <chengpeter37093@gmail.com>
2026-09-07 17:07:49 +0800

    stuff

--- mazegen/output_writer.py
+++ mazegen/output_writer.py
@@ -38,8 +38,8 @@ def write_maze(
             f.write("\n")
 
             # entry / exit
-            f.write(f"{entry[0]},{entry[1]}         #entry    (x,y)\n")
-            f.write(f"{exit[0]},{exit[1]}         #exit    (x,y)\n")
+            f.write(f"{entry[0]},{entry[1]}             (x,y)\n")
+            f.write(f"{exit[0]},{exit[1]}             (x,y)\n")
 
             # path
             f.write(path + "\n")
